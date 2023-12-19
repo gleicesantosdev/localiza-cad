eb7068f205f53c97d7c52e0e7af3d5cd59d7ed7b
gleicesantosdev <116775112+gleicesantosdev@users.noreply.github.com>
2023-12-19 18:26:06 -0300

    add tipagem das  variaveis  do ts

--- localiza/App.tsx
+++ localiza/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { View, Text } from 'react-native';
 import {
@@ -11,30 +11,39 @@ import {
 
 import { styles } from './styles';
 
-//conexão com api 
+// conexão com a api
 const fetchData = async () => {
-    const response = await fetch('http://dados.recife.pe.gov.br/ar/api/3/action/datastore_search?resource_id=1329a80b-c4a6-4ecd-b8e5-09ef75ffa576').then((res)=>res.json()).then((res)=>res.result.records);
-    return response; 
+  try {
+    const response = await fetch('http://dados.recife.pe.gov.br/ar/api/3/action/datastore_search?resource_id=1329a80b-c4a6-4ecd-b8e5-09ef75ffa576');
+    const data = await response.json();
+    return data.result.records;
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+    return [] as Ponto[]; // tipagem 
+  }
 };
 
+console.log(1);
+
+interface Ponto {
+  longitude: number;
+  latitude: number;
+  id: string;
+}
 
 export default function App() {
-
-  const [pontos, setPontos] = useState([]); 
-
+  const [pontos, setPontos] = useState<Ponto[]>([]); // correcao da tipagem
   const [location, setLocation] = useState<LocationObject | null>(null);
-  // acessa o animatte Camera
-  const mapRef = useRef <MapView>(null);
+  const mapRef = useRef<MapView>(null);
 
-  //permissao da camera
+  // permissao p acesso de localizacao
   async function requestLocationPermission() {
     const { granted } = await requestForegroundPermissionsAsync();
 
-    if (granted) { 
+    if (granted) {
       try {
-        const getCurrentPosition = await getCurrentPositionAsync({});
-        setLocation(getCurrentPosition); 
-
+        const currentPosition = await getCurrentPositionAsync({});
+        setLocation(currentPosition);
       } catch (error) {
         console.error('Erro ao obter a localização:', error);
       }
@@ -42,72 +51,76 @@ export default function App() {
       console.warn('Permissão de localização não concedida.');
     }
   }
-  // pega os pontos
- useEffect( () => { 
- async function pegarPonto(){
-  const dados = await fetchData();
-  setPontos(dados);
-}
 
-pegarPonto()
-}, [])
+  // pega pontos
+  useEffect(() => {
+    async function pegarPonto() {
+      const dados = await fetchData();
+      setPontos(dados);
+    }
+
+    pegarPonto();
+  }, []);
 
   useEffect(() => {
     requestLocationPermission();
   }, []);
 
-  //observando rota e precisao da mesma
   useEffect(() => {
-    watchPositionAsync({
-      accuracy: LocationAccuracy.Highest,
-      timeInterval: 1000,
-      distanceInterval: 1 
-    }, (response) => { 
-      //posicao dacamera no mapa 
-      mapRef.current?.animateCamera ({
-        pitch: 70,
-        center:  response.coords
-      })
-      setLocation(response);
-    })
-  }, [])
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (response) => {
+        mapRef.current?.animateCamera({
+          pitch: 70,
+          center: response.coords,
+        });
+        setLocation(response);
+      }
+    );
+  }, []);
 
   return (
-  <View style={styles.container}>
-    {
-      location && 
-    <MapView 
-      ref={mapRef}
-      style = {styles.map}
-      initialRegion={{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.005, // precisao da localizacao 
-        longitudeDelta: 0.005,
-      }}
-    >
-      <Marker
-      //objt p passar latitude e longitude 
-      coordinate={{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      }}
-      />
-      {
-          pontos.map(ponto => { 
-            console.log(ponto) 
-          return <text> tirando erro </text>   
-            //return ( <Marker 
-            //objt p passar latitude e longitude 
-          //coordinate={{
-            //latitude: ponto.latitude,
-           // longitude: ponto.longitude,
-          //}}
-          //image = { require ('./assets/sinal-wifi.png')}
-        //  >)
-        })
-      }
-      </MapView>
-      }
-  </View>);
+    <View style={styles.container}>
+      {location && (
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+          {pontos.map((ponto) => (
+            <Marker
+              key={ponto.id}
+              coordinate={{
+                latitude: ponto.latitude,
+                longitude: ponto.longitude,
+              }}
+              image={require('./assets/sinal-wifi.png')}
+            />
+            
+          ))}
+        </MapView>
+      )}
+      {location && (
+        <Text>
+          Localização Atual: Lat {location.coords.latitude}, Long{' '}
+          {location.coords.longitude}
+        </Text>
+      )}
+    </View>
+  );
 }
